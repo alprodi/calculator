2fef1f68f21f7e8168be410b513b65b33cb14396
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-21 15:34:46 -0400

    Remove unnecessary functions

--- calculator.js
+++ calculator.js
@@ -6,34 +6,17 @@ const subtract = function(x, y) {
 	return (x - y);
 };
 
-const sum = function(sumArray) {
-	let total = 0;
-  for (x = 0; x<sumArray.length; x++){
-    total += sumArray[x];
-  }
-  return total;
+const multiply = function(x, y) {
+
 };
 
-const multiply = function(multArray) {
-  let total = 1;
-  for (x = 0; x<multArray.length; x++){
-    total *= multArray[x];
-  }
-  return total;
-};
+const divide = function(x, y){
 
-const power = function(x, y) {
-  return Math.pow(x, y);
-};
+}
 
-const factorial = function(x) {
-  let cache = 1;
-	if (x === 0 | x === 1) return 1;
-  else if (x > 0){
-    for (i = x; i >1; i--){
-      cache *= i;
-    }
-  return cache;
-  }
-  else return "ERROR"
-};
+const operate = function(){
+
+}
+
+
+
