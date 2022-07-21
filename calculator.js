98f28f8118e35617d67d9a78f578938477437edc
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-21 15:38:54 -0400

    Add basic code for add, subtract, mult, and divide

--- calculator.js
+++ calculator.js
@@ -7,11 +7,13 @@ const subtract = function(x, y) {
 };
 
 const multiply = function(x, y) {
-
+  return(x * y);
 };
 
 const divide = function(x, y){
-
+  if(y == 0){return "Div by Zero"}
+  else{return(x / y);}
+  
 }
 
 const operate = function(){
