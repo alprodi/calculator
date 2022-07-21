3a0062d61e2b161da299719dde31d62704fc5f4a
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-21 15:52:54 -0400

    Remove extra line breaks

--- calculator.js
+++ calculator.js
@@ -2,23 +2,18 @@ let displayValue = "";
 let display = document.querySelector("#calc_display");
 display.innerHTML = displayValue;
 
-
 const add = function(x, y) {
 	return x + y;
 };
-
 const subtract = function(x, y) {
 	return (x - y);
 };
-
 const multiply = function(x, y) {
   return(x * y);
 };
-
 const divide = function(x, y){
   if(y == 0){return "Div by Zero"}
   else{return(x / y);}
-  
 }
 
 const operate = function(operator, x, y){
