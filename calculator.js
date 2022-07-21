ce5d3723e2ad7a057b23766c707197252d22f7e7
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-21 16:06:12 -0400

    Unify button ID names, declare refs in js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,20 @@
 let displayValue = "";
-let display = document.querySelector("#calc_display");
+const display = document.querySelector("#calc_display");
+const calc_0 = document.querySelector("#calc_0");
+const calc_1 = document.querySelector("#calc_1");
+const calc_2 = document.querySelector("#calc_2");
+const calc_3 = document.querySelector("#calc_3");
+const calc_4 = document.querySelector("#calc_4");
+const calc_5 = document.querySelector("#calc_5");
+const calc_6 = document.querySelector("#calc_6");
+const calc_7 = document.querySelector("#calc_7");
+const calc_8 = document.querySelector("#calc_8");
+const calc_9 = document.querySelector("#calc_9");
+const add_button = document.querySelector("#calc_add");
+const subt_button = document.querySelector("#cald_subt");
+const mult_button = document.querySelector("#calc_mult");
+const div_button = document.querySelector("#calc_divide");
+
 display.innerHTML = displayValue;
 
 const add = function(x, y) {
@@ -31,6 +46,4 @@ const operate = function(operator, x, y){
   }
   else {return "Something went wrong."}
 }
-
-
 
