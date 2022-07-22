e98f2099c8311caa37602665dbaa02c5ac9a1f8a
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-21 23:15:46 -0400

    Add button event listeners and test to console.log

--- calculator.js
+++ calculator.js
@@ -1,26 +1,57 @@
 const display = document.querySelector("#calc_display");
-const calc_0 = document.querySelector("#calc_0");
-const calc_1 = document.querySelector("#calc_1");
-const calc_2 = document.querySelector("#calc_2");
-const calc_3 = document.querySelector("#calc_3");
-const calc_4 = document.querySelector("#calc_4");
-const calc_5 = document.querySelector("#calc_5");
-const calc_6 = document.querySelector("#calc_6");
-const calc_7 = document.querySelector("#calc_7");
-const calc_8 = document.querySelector("#calc_8");
-const calc_9 = document.querySelector("#calc_9");
-const add_button = document.querySelector("#calc_add");
-const subt_button = document.querySelector("#cald_subt");
-const mult_button = document.querySelector("#calc_mult");
-const div_button = document.querySelector("#calc_divide");
 
-//object.addEventListener("click", myScript);
+const button_num = document.querySelectorAll('.button_num');
+const button_operator = document.querySelectorAll('.button_operator');
+//const button_operator = document.getElementsByClassName('roundButtons');
 
-let displayValue = "";
-display.innerHTML = displayValue;
-let val1 ="";
+let val1 = "";
 let val2 = "";
 let operator = "";
+let displayValue = "";
+display.innerHTML = displayValue;
+
+// we use the .forEach method to iterate through each button
+button_num.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    //alert(button.id);
+    buttonClick(button.id.replace('calc_', ''));
+  });
+});
+
+button_operator.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    //alert(button.id);
+    buttonClick(button.id.replace('calc_', ''));
+  });
+});
+
+const buttonClick = function(x){
+  console.log(x);
+}
+
+const numClick = function(num){
+  if (val1 == "" && val2 == "" && operator == "") {
+    val1 = num;
+  }
+  else if (val1 != "" && val2 == "" && operator == "") {
+    val1 += num;
+  }
+  else if(operator != "" && val2 == ""){
+    val2 = num;
+  }
+  else if(operator != "" && val2 != ""){
+    val2 += num;
+  }
+  updateDisplay();
+}
+
+const updateDisplay = function(operator, x, y){
+  displayValue = (val1 + operator + val2);
+}
+
+
 
 const add = function(x, y) {
 	return x + y;
